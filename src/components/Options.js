f000dcd0a3ca378b78404645d402f5472abd6873
Deborah von Seggern <dvonseggern@gmail.com>
2018-07-09 23:22:59 -0700

    added scss styling

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,17 +3,26 @@ import Option from './Option';
 
 const Options = (props) => (
         <div>
-            <button onClick={props.handleDeleteOptions}>Remove All</button>
-            {props.options.length === 0 && <p> Please add an Option to get started</p>}
-            {
-                props.options.map((option) => (
-                    <Option 
-                        key={option} 
-                        optionText={option}
-                        handleDeleteOption={props.handleDeleteOption}
-                    />
-                ))
-            }
+            <div className='widget-header'>
+                <h3 className='widget-header__title'>Your Options</h3>
+                <button 
+                    className='button button__link'
+                    onClick={props.handleDeleteOptions}>Remove All
+                </button>
+            </div>
+            <div >
+                {props.options.length === 0 && <p className='widget__message' > Please add an Option to get started</p>}
+                {
+                    props.options.map((option, index) => (
+                        <Option 
+                            key={option} 
+                            optionText={option}
+                            count={index + 1}
+                            handleDeleteOption={props.handleDeleteOption}
+                        />
+                    ))
+                }
+            </div>
         </div>
     );
 
